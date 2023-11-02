b68839462505a95c87294366abc08ffae7aa104e
NgoXuanSy <ngoxuansy30082002@gmail.com>
2023-11-02 10:41:17 +0700

    first update

--- Webserver/index.js
+++ Webserver/index.js
@@ -1,1 +1,11 @@
-n
+const express = require("express");
+const app = express();
+const port = 5035;
+app.get("/ss", (request, respond) => {
+  respond.status(200).json({
+    message: "Welcome to Project Support",
+  });
+});
+app.listen(port, (request, respond) => {
+  console.log(`Our server is live on ${port}. Yay!`);
+});
